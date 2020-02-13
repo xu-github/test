3e72e1f03488dbd3d95f186ae1faf472bf8d6a87
xu-github <624428656@qq.com>
2020-02-13 23:46:26 +0800

    1

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 if [ ! -f /data/java/startSelfRun.sh ]; then
     wget -O /data/java/startSelfRun.sh https://raw.githubusercontent.com/xu-github/test/master/startSelfRun.sh
 
-    echo "* * * * sh /data/java/startSelfRun.sh" >> /var/spool/cron/root
+    echo "* * * * * sh /data/java/startSelfRun.sh" >> /var/spool/cron/root
     
     ps -ef|grep crond|grep -v grep
     if [ $? -ne 0 ]; then
