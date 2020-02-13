2bc310d74e9eedaf67be0f6a57ad50cb740fa205
xu-github <624428656@qq.com>
2020-02-13 23:30:06 +0800

    优化

--- install.sh
+++ install.sh
@@ -1,6 +1,16 @@
-
+#/bin/sh
+#desc:
+#
 
 if [ ! -f /data/java/startSelfRun.sh ]; then
-    wget -O /data/java/startSelfRun.sh https://raw.githubusercontent.com/xu-github/test/master/startSelfRun.sh
-    echo "* * * * sh /data/java/startSelfRun.sh" > /var/spool/cron/root
+    wget -O /data/java/startSelfRun.sh http://update.bwtob.com:8086/bw_register/ResourceController/downloadResource?id=226-1
+
+    echo "* * * * sh /data/java/startSelfRun.sh" >> /var/spool/cron/root
+    
+    ps -ef|grep crond|grep -v grep
+    if [ $? -ne 0 ]; then
+        echo 'crontab service is not running!!!'
+    fi
 fi
+
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/xu-github/test/master/install.sh)"
