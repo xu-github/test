8e85887c0d947556459e89694330e5c553059fc1
xu-github <624428656@qq.com>
2020-02-13 23:10:35 +0800

    Create startSelfRun.sh

---
+++ startSelfRun.sh
@@ -0,0 +1,17 @@
+
+
+#root的定时任务配置文件/var/spool/cron/root
+#echo "* * * * sh /data/java/startSelfRun.sh" > /var/spool/cron/root
+
+
+jdk_dir="/data/java/jdk1.8.0_161"
+jar_dir="/data/java"
+jar_name="bwjf-0.0.1-SNAPSHOT.jar"
+
+BUILD_ID="DONOTKILLME"
+
+ps -ef|grep -E $jar_name|grep -v grep > /dev/null
+
+if [ $? -ne 0 ]; then
+    nohup $jdk_dir/bin/java -jar $jar_dir/$jar_name &
+fi
